0569089d65845f5e2d09b8d7ffdbfd484257d953
JuanK81 <jcasalsbadolato@gmail.com>
2021-11-28 19:46:53 +0100

    Middleware basics

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 // import express from 'express';
 const express = require('express');
+const bodyParser = require('body-parser');
+
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send(`
@@ -17,15 +21,7 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-    req.on('data', data => {
-        const parsed = data.toString('utf8').split('&');
-        const formData = {};
-        for (let pair of parsed) {
-            const [key, value] = pair.split('=');
-            formData[key] = value;
-            console.log(formData); 
-        }
-    })
+    console.log(req.body);
     res.send('account created')
 })
 
