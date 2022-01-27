03995778ec2a4611b14d58f91d21347d2d4cbb02
JuanK81 <jcasalsbadolato@gmail.com>
2022-01-27 12:54:16 +0100

    add getAll method to UsersRepositori class

--- repositories/users.js
+++ repositories/users.js
@@ -1,20 +1,36 @@
-const fs = require("fs");
+const fs = require('fs');
+const { report } = require('process');
 
 class UsersRepository {
-    constructor(filename) {
-        if (!filename) {
-            throw new Error('Creating a repository requires a filename.');
-        }
-
-        //no async code here, this is going to be used only once.
-        this.filename = filename;
-        try {
-            fs.accessSync(this.filename);
-        } catch (err) {
-            fs.writeFileSync(this.filename, '[]');
-        }
+  constructor(filename) {
+    if (!filename) {
+      throw new Error('Creating a repository requires a filename.');
     }
+
+    //no async code here, this is going to be used only once.
+    this.filename = filename;
+    try {
+      fs.accessSync(this.filename);
+    } catch (err) {
+      fs.writeFileSync(this.filename, '[]');
+    }
+  }
+
+  async getAll() {
+    return JSON.parse(await fs.promises.readFile(this.filename, {
+      encoding: 'utf8',
+    }));
+  }
 }
 
 //testing
-new UsersRepository('users.json');
+const test = async () => {
+    const repo = new UsersRepository('users.json');
+
+    const users = await repo.getAll();
+
+    console.log(users);
+}
+
+
+test();
