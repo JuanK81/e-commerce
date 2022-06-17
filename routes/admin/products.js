cfb20f45baa5968f6a8285e0555a58ea94ea0e36
Juan <jcasalsbadolato@gmail.com>
2022-06-17 12:48:05 +0200

    validators for new product

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,16 +1,22 @@
-const express = require('express');
-const ProductsRepository = require('../../repositories/products');
+const express = require("express");
+const { validationResult } = require('express-validator');
 
-const productsNewTemplate = require('../../views/admin/products/new');
+const ProductsRepository = require("../../repositories/products");
+const productsNewTemplate = require("../../views/admin/products/new");
+const { requireTitle, requirePrice } = require("./validators");
 
 const router = express.Router();
 
-router.get('/admin/products', (req, res) => {
-    
+router.get("/admin/products", (req, res) => {});
+
+router.get("/admin/products/new", (req, res) => {
+  res.send(productsNewTemplate({}));
 });
 
-router.get('/admin/products/new', (req, res) => {
-    res.send(productsNewTemplate({}));
+router.post("/admin/products/new", [requireTitle, requirePrice], (req, res) => {
+    const errors = validationResult(req);
+    console.log("*******************", errors, "*******************");
+    res.send('submitted');
 });
 
 module.exports = router;
