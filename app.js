d2d1fa2afed34004c6d2cb1180b2ccf6b05c008e
Juan <jcasalsbadolato@gmail.com>
2022-06-17 12:23:41 +0200

    create products repository, update classes, add new produc from

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
+const productsRouter = require('./routes/admin/products');
 
 const app = express();
+
+app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -14,6 +17,7 @@ app.use(
 );
 
 app.use(authRouter);
+app.use(productsRouter);
 
 app.listen(3000, () => {
   console.log('Listening');
